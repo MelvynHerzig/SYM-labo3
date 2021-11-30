0469b8967b32620b16873070b402ed07bf0d5582
MelvynHerzig <melvyn.herzig@heig-vd.ch>
2021-11-30 16:16:59 +0100

    Code barre et qr code fonctionnel

--- Labo3/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
+++ Labo3/app/src/main/java/ch/heigvd/iict/sym/labo3/MainActivity.kt
@@ -1,12 +1,24 @@
 package ch.heigvd.iict.sym.labo3
 
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 import ch.heigvd.iict.sym.labo3.manipulations.BeaconActivity
-import ch.heigvd.iict.sym.labo3.manipulations.CodeBarreActivity
 import ch.heigvd.iict.sym.labo3.manipulations.NfcActivity
+import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import ch.heigvd.iict.sym.labo3.manipulations.CodeBarreActivity
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
+import com.journeyapps.barcodescanner.ScanOptions
+
 
 /**
  * Activité principale permettant de lancer les 3 manipulations:
@@ -19,6 +31,10 @@ import ch.heigvd.iict.sym.labo3.manipulations.NfcActivity
  */
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+    }
+
     // Référence sur le bouton asynchrone.
     protected lateinit var nfcButton: Button
 
@@ -27,6 +43,7 @@ class MainActivity : AppCompatActivity() {
 
     // Référence sur le bouton sérialisé.
     protected lateinit var codeBarreButton: Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -49,7 +66,42 @@ class MainActivity : AppCompatActivity() {
         }
 
         codeBarreButton.setOnClickListener{
-            startActivity(Intent(this, CodeBarreActivity::class.java))
+            checkPermission(Manifest.permission.CAMERA, MainActivity.CAMERA_PERMISSION_CODE)
+        }
+    }
+
+    /**
+     * Check for permissions
+     * Source: https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
+     */
+    private fun checkPermission(permission: String, requestCode: Int) {
+        if (ContextCompat.checkSelfPermission(this@MainActivity, permission) == PackageManager.PERMISSION_DENIED) {
+
+            // Requesting the permission
+            ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission), requestCode)
+        } else {
+            if(requestCode == CAMERA_PERMISSION_CODE) {
+                startActivity(Intent(this, CodeBarreActivity::class.java))
+            }
+        }
+    }
+
+    /**
+     * Function called when the user accept or decline a permission.
+     * In our case, just check for camera.
+     * Source: https://www.geeksforgeeks.org/android-how-to-request-permissions-in-android-application/
+     */
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>,
+                                            grantResults: IntArray) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (requestCode == CAMERA_PERMISSION_CODE) {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(Intent(this, CodeBarreActivity::class.java))
+                }
+            }
         }
     }
 }
--- Labo3/app/src/main/java/ch/heigvd/iict/sym/labo3/manipulations/CodeBarreActivity.kt
+++ Labo3/app/src/main/java/ch/heigvd/iict/sym/labo3/manipulations/CodeBarreActivity.kt
@@ -1,18 +1,106 @@
 package ch.heigvd.iict.sym.labo3.manipulations
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
 import ch.heigvd.iict.sym.labo3.R
+import com.google.zxing.client.android.BeepManager
+
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+
+import com.google.zxing.BarcodeFormat
+import java.util.*
+import com.google.zxing.ResultPoint
+import android.widget.ImageView
+import android.widget.TextView
+
+import com.journeyapps.barcodescanner.BarcodeResult
+
+import com.journeyapps.barcodescanner.BarcodeCallback
+
+
+
+
 
 /**
  * Activité liée à l'utilisation / détection des codes barres et code QR
+ * Adapté de: https://github.com/journeyapps/zxing-android-embedded/blob/master/sample/src/main/java/example/zxing/ContinuousCaptureActivity.java
  * @author Berney Alec
  * @author Forestier Quentin
  * @author Herzig Melvyn
  */
 class CodeBarreActivity : AppCompatActivity() {
+
+    private lateinit var barcodeView: DecoratedBarcodeView
+    private lateinit var beepManager: BeepManager
+
+    private lateinit var imageView: ImageView
+
+    private lateinit var lastText: TextView
+
+    /**
+     * À la création, initialisation de la vue, récupération des références.
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code_barre)
+
+        barcodeView = findViewById(R.id.code_barre_scanner)
+        val formats: Collection<BarcodeFormat> =
+            Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
+        barcodeView.barcodeView.decoderFactory = DefaultDecoderFactory(formats)
+        barcodeView.initializeFromIntent(intent)
+        barcodeView.decodeContinuous(callback)
+
+        beepManager = BeepManager(this)
+
+        imageView = findViewById(R.id.code_barre_image)
+
+        lastText = findViewById(R.id.code_barre_result_content)
+    }
+
+    /**
+     * Callback lorsqu'un barre code est décodé.
+     */
+    private val callback: BarcodeCallback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult) {
+            if (result.text == null || result.text == lastText.text) {
+                // Prevent duplicate scans
+                return
+            }
+            lastText.text = result.text
+            beepManager.playBeepSoundAndVibrate()
+
+            //Added preview of scanned barcode
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW))
+        }
+
+        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+    }
+
+    /**
+     * À la récupération
+     */
+    override fun onResume() {
+        super.onResume()
+        barcodeView.resume()
+    }
+
+    /**
+     * Mise en pause
+     */
+    override fun onPause() {
+        super.onPause()
+        barcodeView.pause()
+    }
+
+    /**
+     * Sur pression
+     */
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event)
     }
 }
